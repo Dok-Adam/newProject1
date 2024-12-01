6dc24a7e9b7a29bfbd75081873f587ab3f8346d5
Adam <doktoradam72@gmail.com>
2024-12-01 20:04:28 +0500

    delete preview

--- newProject1/ContentView.swift
+++ newProject1/ContentView.swift
@@ -9,24 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         TabView {
             MainView()
                 .tabItem {
                     Label("First", systemImage: "star.fill")
                 }
         }
+        
     }
 }
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-
 
 #Preview {
     ContentView()
